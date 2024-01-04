004623129de7e7d56ca07dbe0bf488b86f5ef1d5
Hugo Carvalho <hhac@protonmail.com>
2024-01-04 00:34:14 +0000

    Added a generic controller class to later be inherited

---
+++ libdb-dotnet/Controllers/CoreController.cs
@@ -0,0 +1,27 @@
+﻿using libdb_dotnet.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace libdb_dotnet.Controllers
+{
+    public class CoreController : Controller
+    {
+        protected async Task<IActionResult> HandleServiceCall<T>(Func<Task<Operation<T>>> serviceMethod) where T : class
+        {
+            try
+            {
+                var result = await serviceMethod();
+                if (!result.IsSuccess)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, new { error = result.Error });
+                }
+
+                return StatusCode(StatusCodes.Status200OK, result.Value);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { error = ex.Message });
+            }
+        }
+    }
+}
