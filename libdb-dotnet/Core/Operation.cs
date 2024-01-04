1f9614060de0085df2212304f986fe27375867e2
Hugo Carvalho <hhac@protonmail.com>
2024-01-04 00:27:21 +0000

    Added a class to control the flow of successful and failed requests

---
+++ libdb-dotnet/Core/Operation.cs
@@ -0,0 +1,26 @@
+﻿namespace libdb_dotnet.Core
+{
+    public readonly struct Operation<T> where T : class
+    {
+        public bool IsSuccess { get; }
+        public string? Error { get; }
+        public T? Value { get; }
+
+        private Operation(bool isSuccess, string? error, T? value)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+            Value = value;
+        }
+
+        public static Operation<T> Success(T value)
+        {
+            return new Operation<T>(true, null, value);
+        }
+
+        public static Operation<T> Fail(string error)
+        {
+            return new Operation<T>(false, error, null);
+        }
+    }
+}
