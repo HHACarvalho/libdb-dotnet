ca8f18411acca6e49756fa9299c2270214b07a7c
Hugo Carvalho <hhac@protonmail.com>
2024-01-05 13:08:40 +0000

    Implemented dependency injection and Cors

--- libdb-dotnet/Program.cs
+++ libdb-dotnet/Program.cs
@@ -1,12 +1,43 @@
+using libdb_dotnet.Core;
+using libdb_dotnet.Repos;
+using libdb_dotnet.Repos.IRepos;
+using libdb_dotnet.Services;
+using libdb_dotnet.Services.IServices;
+using Microsoft.EntityFrameworkCore;
+
+var corsPolicyName = "AllowAllOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicyName, policy =>
+    {
+        policy
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection"));
+    options.UseInMemoryDatabase(databaseName: "libDB");
+});
+
 // Add services to the container.
+
+builder.Services.AddTransient<IBookRepo, BookRepo>();
+builder.Services.AddTransient<IBookService, BookService>();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(corsPolicyName);
 
 app.UseAuthorization();
 
