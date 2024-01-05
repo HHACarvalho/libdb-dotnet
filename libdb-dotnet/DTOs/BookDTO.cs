16c41ee34c7f2ab48a2057459e72f9d680f5931c
Hugo Carvalho <hhac@protonmail.com>
2024-01-05 13:07:34 +0000

    Added BookRequestBody and BookDTO to handle incoming and outgoing data

---
+++ libdb-dotnet/DTOs/BookDTO.cs
@@ -0,0 +1,23 @@
+﻿using libdb_dotnet.Domain;
+
+namespace libdb_dotnet.DTOs
+{
+    public class BookDTOFull
+    {
+        public required string ID;
+        public required string ISBN;
+        public required string Title;
+        public required string Author;
+
+        public static BookDTOFull ToDTO(Book book)
+        {
+            return new BookDTOFull
+            {
+                ID = book.ID.ToString(),
+                ISBN = book.ISBN,
+                Title = book.Title,
+                Author = book.Author
+            };
+        }
+    }
+}
---
+++ libdb-dotnet/DTOs/BookRequestBody.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace libdb_dotnet.DTOs
+{
+    public class BookRequestBody
+    {
+        [Required]
+        [MaxLength(13)]
+        public required string ISBN { get; set; }
+
+        [Required]
+        [MaxLength(96)]
+        public required string Title { get; set; }
+
+        [Required]
+        [MaxLength(48)]
+        public required string Author { get; set; }
+    }
+}
