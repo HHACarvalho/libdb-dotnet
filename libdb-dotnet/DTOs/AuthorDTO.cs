ad62969a9fb4b8a647e0307d8c6e395850c519df
Hugo Carvalho <hhac@protonmail.com>
2024-02-13 21:55:24 +0000

    Joins DTO and RequestBody classes

--- libdb-dotnet/DTOs/AuthorDTO.cs
+++ libdb-dotnet/DTOs/AuthorDTO.cs
@@ -1,11 +1,18 @@
 ﻿using libdb_dotnet.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace libdb_dotnet.DTOs
 {
+    public struct AuthorRequestBody
+    {
+        [RegularExpression(@"^[a-zA-Z0-9\-. ]{0,48}$")]
+        public string Name { get; set; }
+    }
+
     public class AuthorDTOFull
     {
-        public required int Id { get; set; }
-        public required string Name { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
 
         public static AuthorDTOFull ToDTO(Author author)
         {
--- libdb-dotnet/DTOs/BookDTO.cs
+++ libdb-dotnet/DTOs/BookDTO.cs
@@ -1,35 +1,30 @@
 ﻿using libdb_dotnet.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace libdb_dotnet.DTOs
 {
+    public struct BookRequestBody
+    {
+        [RegularExpression(@"^[a-zA-Z0-9\-:'. ]{0,96}$")]
+        public string Title { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int AuthorId { get; set; }
+    }
+
     public class BookDTOFull
     {
-        public required string Isbn { get; set; }
-        public required string Title { get; set; }
-        public required string Author { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
 
         public static BookDTOFull ToDTO(Book book)
         {
             return new BookDTOFull
             {
-                Isbn = book.Isbn,
+                Id = book.Id,
                 Title = book.Title,
                 Author = book.Author.Name
-            };
-        }
-    }
-
-    public class BookDTOPartial
-    {
-        public required string Isbn { get; set; }
-        public required string Title { get; set; }
-
-        public static BookDTOPartial ToDTO(Book book)
-        {
-            return new BookDTOPartial
-            {
-                Isbn = book.Isbn,
-                Title = book.Title
             };
         }
     }
