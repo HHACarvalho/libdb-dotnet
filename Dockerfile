45501e7bc14ee401a99b55102df95dc0cace23ce
Hugo Carvalho <hhac@protonmail.com>
2024-10-28 23:12:02 +0000

    Add a compiling step to the Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,19 @@
+# Use the .NET 8 SDK image
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /app/
+
+# Copy the application files
+COPY libdb-dotnet/ ./
+
+# Build the application
+RUN dotnet publish -o /app/publish/
+
 # Use the ASP.NET Core runtime image
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS runtime
 WORKDIR /app/
 
 # Copy the published app to the docker image
-COPY libdb-dotnet/bin/Release/net8.0/publish/ ./
+COPY --from=build /app/publish/ ./
 
 # Copy the .env file
 COPY .env ./
--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Build the application
-dotnet publish
+# Create a migration and update the database
+dotnet-ef migrations add Initial --project libdb-dotnet/
+dotnet-ef database update --project libdb-dotnet/
 
 # Build the docker image
 docker build -t libdb-dotnet-image:latest ./
@@ -11,10 +12,6 @@ docker stop libdb-dotnet-container && docker rm libdb-dotnet-container
 
 # Create and run the docker container
 docker run -d -p 5000:8080 --network libdb-network --name libdb-dotnet-container libdb-dotnet-image:latest
-
-# Create a migration and update the database
-dotnet-ef migrations add Initial --project libdb-dotnet/
-dotnet-ef database update --project libdb-dotnet/
 
 # Delete the local migration files
 rm -r libdb-dotnet/migrations/
