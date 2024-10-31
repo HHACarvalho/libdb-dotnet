f835de42ef37b7635443395823774177adb547ec
Hugo Carvalho <hhac@protonmail.com>
2024-10-31 19:13:38 +0000

    Remove the migration from publish.sh

--- publish.sh
+++ publish.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Create a migration and update the database
-dotnet-ef migrations add Initial --project libdb-dotnet/
-dotnet-ef database update --project libdb-dotnet/
-
 # Build the docker image
 docker build -t libdb-dotnet-image:latest ./
 
@@ -12,6 +8,3 @@ docker stop libdb-dotnet-container && docker rm libdb-dotnet-container
 
 # Create and run the docker container
 docker run -d -p 5000:8080 --network libdb-network --name libdb-dotnet-container libdb-dotnet-image:latest
-
-# Delete the local migration files
-rm -r libdb-dotnet/migrations/
