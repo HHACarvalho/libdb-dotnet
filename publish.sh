444e13558724fee9d5bb1c5376122eb26add086d
Hugo Carvalho <hhac@protonmail.com>
2024-11-07 20:42:39 +0000

    Restores the publishing shell script

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build the docker image
+docker build -t libdb-dotnet-image:latest ./
+
+# Stop and remove any existing docker container
+docker stop libdb-dotnet-container && docker rm libdb-dotnet-container
+
+# Create and run the docker container
+docker run -d -p 8080:8080 --network libdb-network --name libdb-dotnet-container -e ENVIRONMENT=PRODUCTION libdb-dotnet-image:latest
